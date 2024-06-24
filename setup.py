01efb9f4fb5173d5129c36bfcf388f66d679249b
Joshiwa van Marrewijk <56585136+Joshiwavm@users.noreply.github.com>
2024-06-24 12:54:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="jacked", 
     version="0.1.0",
     author="Joshiwa van Marrewijk",
     author_email="joshiwa01@gmail.com",
     description="Jackknifing interferometric datasets",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Joshiwavm/jacked", 
     packages=find_packages(),
